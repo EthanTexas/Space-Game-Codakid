352d871cc3b43f1ad175b347010185e71124500c
EthanTexas <ethanyweiss@gmail.com>
2024-07-19 16:15:31 -0500

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -4,7 +4,7 @@ class EnemyGroup extends Phaser.Physics.Arcade.Group {
     this.createMultiple({
       key: 'enemy',
       frame: 0,
-      repeat: 100,
+      repeat: 99,
       setActive: false,
       setVisible: false,
     });
